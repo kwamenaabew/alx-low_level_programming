cd533fb28d4411f8931bbcec7f004a0b912616dd
kwamenaabew <davidabewbaidoo.gmail.com>
2022-10-14 00:52:43 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 #include <stdio.h>
+   /* This program prints ou one line */
 
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle");
-	retur (0):
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
