9c4a435d3af4e8cf503f7f8264f9854a3f498bf1
kwamenaabew <davidabewbaidoo.gmail.com>
2022-10-14 13:24:15 -0700

    final

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - prints out a string 
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long int j;
+	long long int k;
+	char l;
+	float m;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(j));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(m));
+	return (0):
+}
