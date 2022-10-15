42dd62cc5a49975ff0caf23f0eefb929f1ef956d
kwamenaabew <davidabewbaidoo.gmail.com>
2022-10-15 10:32:02 -0700

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *
+ *main - Entry point
+ *
+ *Return: 0
+ *
+ */
+
+int main(void)
+{
+	char aaa;
+
+	for (aaa = 'a'; aaa <= 'z'; aaa++)
+	{
+		putchar(aaa);
+	}
+
+	for (aaa = 'A'; aaa <= 'Z'; aaa++)
+	{
+		putchar(aaa);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
