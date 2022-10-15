4e7effbb7be373a821fd27588e34c39cb05d4fd1
kwamenaabew <davidabewbaidoo.gmail.com>
2022-10-15 00:20:33 -0700

    redone

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,12 +24,12 @@ int main(void)
 		printf(" and is greater than 5\n");
 	}
 
-	if (n % 10 == 0)
+	else if (n % 10 == 0)
 	{
 		printf(" and is 0\n");
 	}
 
-	if (n % 10 < 6 && n % 10 != 0)
+	else (n % 10 < 6 && n % 10 != 0)
 	{
 		printf(" and is less than 6 and not 0\n");
 	}
