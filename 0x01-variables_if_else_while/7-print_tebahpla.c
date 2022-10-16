a570c988505ce6027a06b001197b57dfb11065a5
kwamenaabew <davidabewbaidoo.gmail.com>
2022-10-15 22:45:12 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This program prints out alphabet
+ * in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	char aaa;
+
+	for (aaa = 'z'; aaa > 'a'; aaa++)
+	{
+		putchar(aaa);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
