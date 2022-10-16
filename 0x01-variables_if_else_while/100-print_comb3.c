27dde0b8c541809e216626c8cb982c78c5544d89
kwamenaabew <davidabewbaidoo.gmail.com>
2022-10-16 09:13:16 -0700

    brains

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This program prints all possible combinations
+ * of two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	int j;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = 1; i <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (i == '8' && j == '9')
+				{
+					continue;
+				}
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
