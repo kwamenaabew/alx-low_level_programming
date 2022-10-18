6fdf1c2d82c9ee55da09174747c0321815fdf512
kwamenaabew <davidabewbaidoo.gmail.com>
2022-10-18 16:14:10 -0700

    alpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,17 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include  <stdio.h>
 
 int _putchar(int i);
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+
+	}
+	putchar('\n');
+}
 #endif
