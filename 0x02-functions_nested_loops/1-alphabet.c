838d559871b1c93106ad03650032b926ce858bff
kwamenaabew <davidabewbaidoo.gmail.com>
2022-10-18 16:21:59 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return. Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-#include  <stdio.h>
+
 
 int _putchar(int i);
 void print_alphabet(void)
-{
-	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-
-	}
-	putchar('\n');
-}
 #endif
