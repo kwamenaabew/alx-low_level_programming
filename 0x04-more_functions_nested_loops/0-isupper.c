51747f3369c69e7915312621052d88c7cc130f75
kwamenaabew <davidabewbaidoo.gmail.com>
2022-10-20 14:31:19 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - main function
+ * This program returns 1 if
+ * @c: is uppercase or 0 if otherwise
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	char x;
+
+	for (x = 'A'; x <= 'Z'; z++)
+	{
+		if (c == x)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+* Nested loops and how to use them
+* +functions and how to use them
+* Difference between a declaration and a definition of a function
+*etc
