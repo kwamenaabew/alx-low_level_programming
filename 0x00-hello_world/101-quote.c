504c33b2022619aa186d280628c5bd5cca5406a4
kwamenaabew <davidabewbaidoo@gmail.com>
2022-12-08 14:14:28 +0000

    Using the write funtion

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 
 /**
- *main - entry point
- *Return - 0
+ *main - entry point, This program uses write
+ *to print output
+ *Return: 1 Changed to 1
  */
 
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piec of art is useful
+	write(STDOUT_FILENO, "and that piece of art is useful
+	      \" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
