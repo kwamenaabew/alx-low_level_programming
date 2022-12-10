1b5b1707a9a9e4e58e7bdcd428fa2e6cc84380d2
kwamenaabew <davidabewbaidoo@gmail.com>
2022-12-10 23:48:36 +0000

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+*
+*main - Entry point
+*
+*Return: 0
+*
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+
+		for (j = '1'; j <= '9'; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+			if (i > j)
+			{
+				continue;
+			}
+			putchar(i);
+			putchar(j);
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
