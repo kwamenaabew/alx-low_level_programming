3f216bda367096a822ce8f6a7bc4b9d6062c211d
kwamenaabew <davidabewbaidoo@gmail.com>
2022-12-16 20:23:36 +0000

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-	unsigned long int i = 3, n = 612852475143;
+	unsigned long int i, x = 612852475143;
 
-	for (; i < 12057; i += 2)
+	for (i = 3; i < 12057; i += 2)
 	{
-		while (n % i == 0 && n != i)
-			n /= i;
+		while (x % i == 0 && x != i)
+		{
+			x /= i;
+		}
 	}
-	printf("%lu\n", n);
+	printf("%lu\n", x);
 	return (0);
 }
