eaf0b918dc95115a9e4f3917163a99418c9905e6
kwamenaabew <davidabewbaidoo@gmail.com>
2022-12-19 23:16:18 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int _strlen(char *s);
+/**
+ * print_rev - This function prints a string,
+ * in reverse, followed by a new line.
+ * @s: string parameter
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int n = _strlen(s);
+
+	int i;
+
+	for (i = 0; i <= n; n--)
+	{
+		putchar(s[n]);
+	}
+	putchar('\n');
+}
+
+/**
+ * _strlen - function prints out the length of string
+ * @s: string parameter
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\n')
+	{
+		n++;
+	}
+
+	return (n);
+}
