2e77e48bbe634e50da38e5131f5954bd7fd94480
kwamenaabew <davidabewbaidoo@gmail.com>
2022-12-21 15:33:36 +0000

    strcom fic

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,18 +13,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while ((*s1 != '\0') && (*s2 != '\0'))
+	while (*s1 && *s2)
 	{
-		if (*s1 > *s2)
+		if (*s1 != *s2)
 		{
-			return (1);
+			return (*s1 - *s2);
 		}
-		if (*s1 < *s2)
-		{
-			return (-1);
-		}
+
 		s1++;
 		s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
