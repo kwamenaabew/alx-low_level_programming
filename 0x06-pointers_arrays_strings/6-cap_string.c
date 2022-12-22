891c1b98e468edadd5f190777512eba8ceb8f132
kwamenaabew <davidabewbaidoo@gmail.com>
2022-12-22 12:22:02 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string parameter
+ * Return: string capitalised
+ */
+
+char *cap_string(char *s)
+{
+	
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * This function that encodes a string into 1337.
+ * @s: sting parameter
+ * Return: encoded string.
+ */
+
+char *leet(char *s)
+{
+	int a, b;
+
+	char numbers[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char letters[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	while (s[a])
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (s[a] == letters[b])
+			{
+				s[a] = numbers[b];
+			}
+		}
+		a++;
+	}
+	return (s);
+}
