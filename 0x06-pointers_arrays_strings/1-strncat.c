3d3f250fdd74fe6959b775566974bc2683b79420
kwamenaabew <davidabewbaidoo@gmail.com>
2022-12-23 12:12:23 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,18 +11,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-  int i = 0, j = 0;
+	int i = 0, j = 0;
 
-  while (dest[i] != '\0')
-    {
-      i++;
-    }
-  while (j < n && src[j] != '\0')
-    {
-      dest[i] = src[j];
-      i++;
-      j++;
-    }
-  dest[i] = '\0';
-  return (dest);
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
