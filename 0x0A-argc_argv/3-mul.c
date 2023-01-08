a6fe729026cbc7bb44c8454d134e7e5a3112e380
kwamenaabew <davidabewbaidoo@gmail.com>
2023-01-08 19:19:22 +0000

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This program multiplies two numbers
+ * if less than two numbers are inputs prints Error
+ * @argc: arg count
+ * @argv: arg vector array
+ * Return: 1 if error .
+ */
+
+int main(int argc, *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d", argv[1] * argv[2]);
+		return (0);
+	}
+}
