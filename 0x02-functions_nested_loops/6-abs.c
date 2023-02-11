caee80b554a255ba4feca239cf194fbd3a5f4a93
kwamenaabew <davidabewbaidoo@gmail.com>
2023-02-11 17:40:58 +0000

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -12,13 +12,13 @@
 
 int _abs(int n)
 {
-	if (n >= 0)
+	while (n != 0)
 	{
-		return (n);
-	}
-	else
-	{
-		return (n * -1);
-	}
+		if (n < 0)
+			return (n * -1);
+		if (n > 0)
+			return (n);
 
+	}
+	return (0);
 }
