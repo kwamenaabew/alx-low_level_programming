46a7966600584e8e149a0b68063619a4b395eade
kwamenaabew <davidabewbaidoo@gmail.com>
2023-02-25 18:40:30 +0000

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,12 +11,17 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int i, ci;
+	int i;
+	int ci;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 		i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
